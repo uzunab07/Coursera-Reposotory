549dbcbae602139d187e308e5a3c93c509b32e71
Khaled Mohamed Ali <mohakedk812.ka@gmail.com>
2024-05-24 00:02:52 -0400

    Views and stored procedures

---
+++ IBM Data Science/SQL and Python/HR_Database_Create_Tables_Script.sql
@@ -0,0 +1,81 @@
+DROP TABLE IF EXISTS EMPLOYEES;
+DROP TABLE IF EXISTS JOB_HISTORY;
+DROP TABLE IF EXISTS JOBS;
+DROP TABLE IF EXISTS DEPARTMENTS;
+DROP TABLE IF EXISTS LOCATIONS;
+
+
+
+CREATE TABLE EMPLOYEES (
+                          EMP_ID CHAR(9) NOT NULL,
+                          F_NAME VARCHAR(15) NOT NULL,
+                          L_NAME VARCHAR(15) NOT NULL,
+                          SSN CHAR(9),
+                          B_DATE DATE,
+                          SEX CHAR,
+                          ADDRESS VARCHAR(30),
+                          JOB_ID CHAR(9),
+                          SALARY DECIMAL(10,2),
+                          MANAGER_ID CHAR(9),
+                          DEP_ID CHAR(9) NOT NULL,
+                          PRIMARY KEY (EMP_ID)
+                        );
+
+CREATE TABLE JOB_HISTORY (
+                            EMPL_ID CHAR(9) NOT NULL,
+                            START_DATE DATE,
+                            JOBS_ID CHAR(9) NOT NULL,
+                            DEPT_ID CHAR(9),
+                            PRIMARY KEY (EMPL_ID,JOBS_ID)
+                          );
+
+CREATE TABLE JOBS (
+                    JOB_IDENT CHAR(9) NOT NULL,
+                    JOB_TITLE VARCHAR(30) ,
+                    MIN_SALARY DECIMAL(10,2),
+                    MAX_SALARY DECIMAL(10,2),
+                    PRIMARY KEY (JOB_IDENT)
+                  );
+
+CREATE TABLE DEPARTMENTS (
+                            DEPT_ID_DEP CHAR(9) NOT NULL,
+                            DEP_NAME VARCHAR(15) ,
+                            MANAGER_ID CHAR(9),
+                            LOC_ID CHAR(9),
+                            PRIMARY KEY (DEPT_ID_DEP)
+                          );
+
+CREATE TABLE LOCATIONS (
+                          LOCT_ID CHAR(9) NOT NULL,
+                          DEP_ID_LOC CHAR(9) NOT NULL,
+                          PRIMARY KEY (LOCT_ID,DEP_ID_LOC)
+                        );
+
+-- VIEWS
+
+CREATE OR REPLACE VIEW EMPDEPT AS 
+SELECT EMP_ID, F_NAME, L_NAME, B_DATE, `DEP_ID`
+FROM `EMPLOYEES`;
+
+DROP VIEW `empsalary`
+
+CREATE OR REPLACE VIEW EMPDEPT AS
+SELECT EMP_ID, F_NAME, L_NAME, DEP_NAME
+FROM EMPLOYEES, DEPARTMENTS
+WHERE EMPLOYEES.DEP_ID = DEPARTMENTS.DEPT_ID_DEP;
+
+
+-- STORED PROCEDURES
+DELIMITER //
+
+CREATE PROCEDURE GET_ALL_EMP_JOBS()
+
+BEGIN
+    SELECT * FROM `EMPLOYEES` LIMIT 2;
+    SELECT * FROM `JOBS` LIMIT 3;
+END 
+//
+
+DELIMITER;
+
+CALL GET_ALL_EMP_JOBS;
